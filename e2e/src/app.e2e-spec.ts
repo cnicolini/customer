ac40da76abb1c40e3ecdb5450625b9c6bed5e426
Christian Nicolini <nicolini.christian@gmail.com>
2018-11-28 05:44:39 -0500

    e2e test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display page title', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to customer!');
+    expect(page.getParagraphText()).toEqual('Customers');
   });
 });
