620339a48eaba7084fee0c285f9a4d1843872ac5
Christian Nicolini <35670152+cnicolini@users.noreply.github.com>
2018-12-01 14:00:39 -0500

    Update hooks.js

--- e2e/utilities/hooks.js
+++ e2e/utilities/hooks.js
@@ -7,7 +7,6 @@ Before(function(scenario) {
 
 After(function(testCase) {
   if (testCase.result.status === Status.FAILED) {
-    console.log(`After - Status - ${testCase.name}`);
     world = this;
     return browser.takeScreenshot().then(function(screenShot) {
       // screenShot is a base-64 encoded PNG
