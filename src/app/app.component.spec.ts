bb1b97c96c14efcdf89dfabddecf40b46109716e
Christian Nicolini <CNicolini@yprime.com>
2018-12-07 22:11:34 +0000

    Tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+
 import { AppComponent } from './app.component';
+import { MessagesComponent } from './messages/messages.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -9,7 +11,8 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        MessagesComponent
       ],
     }).compileComponents();
   }));
@@ -20,16 +23,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'customer'`, () => {
+  it(`should have as title 'Customers'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('customer');
+    expect(app.title).toEqual('Customers');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to customer!');
+    expect(compiled.querySelector('h1').textContent).toContain('Customers');
   });
 });
--- src/app/customer/customer.component.spec.ts
+++ src/app/customer/customer.component.spec.ts
@@ -1,20 +1,36 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
-import { CustomersComponent } from './customers.component';
+import { Customer } from '../customer';
+import { CustomerComponent } from './customer.component';
+import { CustomerService } from '../customer.service';
 
-describe('CustomersComponent', () => {
-  let component: CustomersComponent;
-  let fixture: ComponentFixture<CustomersComponent>;
+describe('CustomerComponent', () => {
+  let component: CustomerComponent;
+  let fixture: ComponentFixture<CustomerComponent>;
+
+  const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+  const customerServiceSpy = jasmine.createSpyObj('CustomerService', ['getCustomer', 'deleteCustomer'])
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CustomersComponent ]
+      imports: [
+        RouterTestingModule
+      ],
+      providers: [
+        { provide: CustomerService, useValue: customerServiceSpy },
+        { provide: Router, useValue: routerSpy }
+      ],
+      declarations: [
+        CustomerComponent
+      ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CustomersComponent);
+    fixture = TestBed.createComponent(CustomerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
